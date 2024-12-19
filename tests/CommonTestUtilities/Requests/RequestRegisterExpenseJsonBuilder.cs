37814792bf0fa064388f4a6dd718a886dbe65334
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2024-12-19 18:12:51 -0300

    feat: add tests unit with FluentAssertions and bogus

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,18 @@
+using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>("pt_BR")
+            .RuleFor(x => x.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(x => x.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(x => x.Date, faker => faker.Date.Past())
+            .RuleFor(x => x.Amount, faker => faker.Random.Decimal(min: 1, max: 100))
+            .RuleFor(x => x.PaymentType, faker => faker.PickRandom<PaymentType>());
+
+    }
+}
