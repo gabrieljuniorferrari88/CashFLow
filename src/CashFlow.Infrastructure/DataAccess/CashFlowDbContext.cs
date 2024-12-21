963a6ac7dcb27269fc0009f3d5e03e240696f837
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2024-12-21 18:33:42 -0300

    feat: configuration injection dependency

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -2,7 +2,7 @@ using CashFlow.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 namespace CashFlow.Infrastructure.DataAccess;
 
-public class CashFlowDbContext : DbContext
+internal class CashFlowDbContext : DbContext
 {
     public DbSet<Expense> Expenses { get; set; }
 
