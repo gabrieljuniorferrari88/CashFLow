045d7e856c98f7fa686442fcf3ee98f513ac34e5
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2024-12-26 15:36:38 -0300

    refactor: adjusted interfaces and functions with async and await

--- src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -10,5 +10,5 @@ internal class ExpensesRepository : IExpensesRepository
     {
         _dbContext = dbContext;
     }
-    public void Add(Expense expense) => _dbContext.Expenses.Add(expense);
+    public async Task Add(Expense expense) => await _dbContext.Expenses.AddAsync(expense);
 }
--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,4 @@
-using CashFlow.Application.UseCases.Expenses;
+using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Enums;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
