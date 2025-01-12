c085a74bcac1badd87d63e29e8870a77f2233d74
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2025-01-12 15:37:45 -0300

    feat: add endpoint login user and generate token with jwt and bcrypt

--- src/CashFlow.Domain/Repositories/User/IUserReadOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -3,4 +3,5 @@ namespace CashFlow.Domain.Repositories.User;
 public interface IUserReadOnlyRepository
 {
     Task<bool> ExistActiveUserWhiteEmail(string email);
+    Task<Entities.User?> GetUserByEmail(string email);
 }
--- src/CashFlow.Infrastructure/Security/Cryptography/BCrypt.cs
+++ src/CashFlow.Infrastructure/Security/Cryptography/BCrypt.cs
@@ -1,7 +1,6 @@
 using CashFlow.Domain.Security.Cryptography;
 using BC = BCrypt.Net.BCrypt;
-
-namespace CashFlow.Infrastructure.Security;
+namespace CashFlow.Infrastructure.Security.Cryptography;
 
 internal class BCrypt : IPasswordEncripter
 {
@@ -10,4 +9,6 @@ internal class BCrypt : IPasswordEncripter
         string passwordHash = BC.HashPassword(password);
         return passwordHash;
     }
+    
+    public bool Verify(string password, string passwordHash) => BC.Verify(password, passwordHash);
 }
